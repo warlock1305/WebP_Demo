0b4e3b4a60c8a3543a4c9f7936bf85ba578e4261
Warlock1305 <alimarat131@gmail.com>
2024-03-27 16:46:41 +0100

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// const bookscontainer = document.getElementById('AllBooks')
+
+const bookscontainer = document.getElementById('AllBooks')
 console.log(bookscontainer)
-/// fetch
-// populate container
+
 $.get("../books.json", (data, status) => {
 
     data.forEach(book => {
